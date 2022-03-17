5aacb2a177691cbe97c8d78c30b08c341170cb6f
Mad Scud <mddscudder44@gmail.com>
2022-03-17 16:36:18 -0400

    added fetch and json

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //const { formSubmission } = require("./scriptHelper");
 
+//const { addDestinationInfo } = require("./scriptHelper");
+
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
     let form = document.querySelector("form");
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function(event){
         event.preventDefault();
         let list = document.getElementById("faultyItems");
         let pilot = document.querySelector("input[name=pilotName]").value;
@@ -24,12 +26,21 @@ window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
+
+       let planet = pickPlanet(listedPlanets);
+       let name = planet.name;
+       let diameter = planet.diameter;
+       let star = planet.star;
+       let distance = planet.distance;
+       let imageUrl = planet.image;
+       let moons = planet.moons;
+       addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
       
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    }) 
